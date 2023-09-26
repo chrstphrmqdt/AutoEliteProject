23d1ff766b62e03b019402fcb8a513e495f4a0ba
crit <c.maquidato.538613@umindanao.edu.ph>
2023-09-26 14:26:51 +0800

    initial

--- src/main/java/com/example/autoeliteproject/carcardsController.java
+++ src/main/java/com/example/autoeliteproject/carcardsController.java
@@ -1,10 +1,16 @@
 package com.example.autoeliteproject;
 
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class carcardsController {
 
@@ -22,5 +28,23 @@ public class carcardsController {
 
     @FXML
     private Button detailsBtn;
+    
+    //event handlers
+    @FXML
+    private void handleDetailsButtonClick() {
+        loadScene("cardetails.fxml");
+    }
 
+    public void loadScene(String sceneFXML) {
+        try {
+            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource(sceneFXML));
+            Parent root = fxmlLoader.load();
+            Scene scene = new Scene(root);
+            Stage stage = (Stage) detailsBtn.getScene().getWindow();
+            stage.setScene(scene);
+            stage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
